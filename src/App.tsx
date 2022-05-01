23909faf3810899ed94ab379bfbbcb8e21773361
sbv <38765146+stephanBV@users.noreply.github.com>
2022-05-01 12:21:25 +0200

    fixed Switch error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-// import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 function App() {
   return (
-    <div>
-        <h1>TEst</h1>
+    <div className="app">
+        <div className='navbar'>
+
+        </div>
+        <div className='main'>
+
+        </div>
+        <div className='footer'>
+            
+        </div>
+        
     </div>
   );
 }
